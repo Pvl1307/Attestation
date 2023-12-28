39cdadb7bd10c0aeedb6ee0a54ab22e95da94fe4
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-12-28 19:59:36 +0300

    suppliers' views.py

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from suppliers.models import Contacts, Product, Network
+from suppliers.serializers import ContactsSerializer, ProductSerializer, NetworkSerializer
+
+
+class ContactsViewSet(ModelViewSet):
+    """Эндпоинт для контактов"""
+    serializer_class = ContactsSerializer
+    queryset = Contacts.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class ProductViewSet(ModelViewSet):
+    """Эндпоинт для продуктов"""
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class NetworkCreateAPIView(generics.CreateAPIView):
+    serializer_class = NetworkSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class NetworkListAPIView(generics.ListAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+
+class NetworkRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+
+class NetworkUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class NetworkDestroyAPIView(generics.DestroyAPIView):
+    queryset = Network.objects.all()
