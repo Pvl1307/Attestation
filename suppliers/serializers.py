b642d4614f8e51b6309d77cb9bd96467986dca81
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-12-28 20:45:45 +0300

    validation for updating 'debt_to_supplier' field

--- suppliers/serializers.py
+++ suppliers/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from suppliers.models import Contacts, Product, Network
+from suppliers.validators import forbidden_change_debt
 
 
 class ContactsSerializer(serializers.ModelSerializer):
@@ -19,3 +20,12 @@ class NetworkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Network
         fields = '__all__'
+
+    def validate(self, attrs):
+        instance = self.instance
+
+        old_debt = instance.debt_to_supplier if instance else None
+        new_debt = attrs.get('debt_to_supplier', old_debt)
+
+        forbidden_change_debt(new_debt, old_debt)
+        return attrs
