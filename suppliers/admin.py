91ee22d69c17f8fdda7e5e3cf83686adbd7396a4
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-12-30 02:25:33 +0300

    admin panel gives suppliers' url with data

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from suppliers.models.contacts_model import Contacts
 from suppliers.models.network_model import Network
@@ -17,9 +19,19 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Network)
 class NetworkAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'contacts', 'supplier', 'debt_to_supplier', 'creation_date',)
+    list_display = ('pk', 'name', 'contacts', 'get_supplier', 'debt_to_supplier', 'creation_date',)
     list_filter = ('contacts__city',)
     actions = ('clear_the_debt',)
+    list_display_links = ('get_supplier',)
+
+    def get_supplier(self, obj):
+        supplier = obj.supplier
+        if supplier:
+            url = reverse('admin:suppliers_network_change', args=[supplier.id])
+            return format_html('<a href="{}">{}</a>', url, supplier.name)
+        return '-'
+
+    get_supplier.short_description = 'Поставщик'
 
     @admin.action(description='Обнулить задолжность')
     def clear_the_debt(self, _, queryset):
